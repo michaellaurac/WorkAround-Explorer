3de4baa5b5519f8bd46320ae137e4178feca5f26
Michaël Laurac <michaellaurac@gmail.com>
2022-03-20 22:21:25 +0100

    Called to the appropriate imported function from workAroundModule.js using either (or both) company and role as arguments

--- main.js
+++ main.js
@@ -55,10 +55,10 @@ function updateResults(){
   if (!company || !role) { return; }
 
   // TODO: Use the workAroundModule functions to calculate the needed data.
-  const averageSalaryByRole = 0;
-  const averageSalaryByCompany = 0;
-  const salary = 0;
-  const industryAverageSalary = 0;
+  const averageSalaryByRole = getAverageSalaryByRole(role);
+  const averageSalaryByCompany = getAverageSalaryByCompany(company);
+  const salary = getSalaryAtCompany(role, company);
+  const industryAverageSalary = getIndustryAverageSalary();
 
   // Render them to the screen.
   document.getElementById('salarySelected').innerText = `The salary for ${role}s at ${company} is \$${salary}`;
